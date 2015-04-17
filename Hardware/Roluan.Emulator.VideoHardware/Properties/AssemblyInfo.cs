a30406c1647291b225e176150a9684277929945f
rostislavrotaru <rostislav.rotaru@roluan.com>
2015-04-17 15:26:00 +0300

    Added video hardware

---
+++ Hardware/Roluan.Emulator.VideoHardware/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Roluan.Emulator.VideoHardware")]
+[assembly: AssemblyProduct("Roluan.Emulator.VideoHardware")]
+[assembly: AssemblyCopyright("Copyright © Roluan 2015")]
+[assembly: AssemblyVersion ("1.0.*")]
