3e966eb44a419876e8a04d63457ff64b8aceb6e1
Rostislav Rotaru <rostislav@rotaru.me>
2015-11-19 13:35:35 +0200

    Initialize a test Tryte in Main

--- Roluan.Emulator/Program.cs
+++ Roluan.Emulator/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace Roluan.Emulator
+﻿using Roluan.Emulator.Common;
+
+namespace Roluan.Emulator
 {
     public class Program
     {
         public void Main(string[] args)
         {
+            Tryte t = new Tryte();
         }
     }
 }
